a5cd6561a977e1dccf70d7398780cc106251d97f
h4ck32m4n <artus.gosselin.1995@gmail.com>
2020-01-31 22:28:27 +0100

    UPDATE : gotools add static demo

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"os"
 
 	"github.com/h4ck32m4n/gotools/digger"
+	"github.com/h4ck32m4n/gotools/static"
 )
 
 func main() {
@@ -17,7 +18,7 @@ func main() {
 
 	staticCmd := flag.NewFlagSet("static", flag.ExitOnError)
 	staticSilent := staticCmd.Bool("silent", false, "silent")
-	staticBuild := staticCmd.Bool("build", false, "build static")
+	staticFolder := staticCmd.String("folder", digger.Home(), "build static in folder")
 
 	if len(os.Args) < 2 {
 		fmt.Println("expected 'digger' or 'static' subcommands")
@@ -42,8 +43,8 @@ func main() {
 		if *diggerTarget != "" {
 			digger.Build(*diggerTarget)
 			root.Build(*diggerTarget, 0)
-			temp := digger.Dig(*diggerTarget)
 			if !*diggerSilent {
+				temp := digger.Dig(*diggerTarget)
 				println(temp.Tree())
 			}
 		}
@@ -55,8 +56,23 @@ func main() {
 		if !*diggerSilent {
 			fmt.Println("subcommand 'static'")
 			fmt.Println("  verbose:", *staticSilent)
-			fmt.Println("  build:", *staticBuild)
+			fmt.Println("  folder:", *staticFolder)
 			fmt.Println("  tail:", staticCmd.Args())
+		}
+		header := "<!doctype html>\n<html>\n<head>\n"
+		header += "<meta name=\"viewport\" content=\"width=device-width\"/>\n"
+		header += "<meta charset=\"utf-8\">\n<title>" + "Title" + "</title>\n"
+		header += "<link href=\"main.css\" rel=\"stylesheet\">\n</head>\n"
+		body := "<body>\n<h1>" + "h1" + "</h1>\n"
+		body += "<hr><h2>" + "h2" + "</h2><hr>"
+		footer := "</body>\n</html>"
+		css := "body { width: device-width; }\n"
+		css += "h1 { width: device-width; font: 500 80px/1.5 Helvetica, Verdana, sans-serif; margin: 0; padding: 0; }\n"
+		css += "h2 { width: device-width; font: 500 50px/1.5 Helvetica, Verdana, sans-serif; margin: 0; padding: 0; }\n"
+		static.Make(*staticFolder, css, header, body, footer).Build()
+		temp := digger.Dig(*staticFolder)
+		if !*staticSilent {
+			println(temp.Tree())
 		}
 	default:
 		fmt.Println("expected 'digger' or 'static' subcommands")
